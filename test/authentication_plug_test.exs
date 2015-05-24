060e1dc3650a4806ec51770e247f5903f6212391
Shankar Dhanasekaran <shankar@opendrops.com>
2015-05-25 01:34:41 +0530

    remove unwanted tests
    Plugs can be selectively included/excluded for a particular action using the 'when' guard while calling the plug. So there is no need for these tests.

--- test/authentication_plug_test.exs
+++ test/authentication_plug_test.exs
@@ -5,56 +5,6 @@ defmodule AuthenticationPlugTest do
   alias Passport.AuthenticationPlug
   alias Passport.SessionHelper
 
-  @default_opts AuthenticationPlug.init([])
-  @only_opts AuthenticationPlug.init([only: [:index]])
-  @except_opts AuthenticationPlug.init([except: [:index]])
-  @both_opts AuthenticationPlug.init([except: [:index], only: [:index]])
-  @custom_flash_key AuthenticationPlug.init([flash_key: :warning])
-
-  test "it checks if the user is logged in if the options are default" do
-    with_mock Passport.SessionManager, [:passthrough], [logged_in?: fn(_conn) -> true end] do
-    conn = conn(:get, "/")
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> SessionHelper.session_with_user
-      |> AuthenticationPlug.call(@default_opts)
-
-    assert called Passport.SessionManager.logged_in?(conn)
-    end
-  end
-
-  test "it checks if the user is logged in if only only action" do
-    with_mock Passport.SessionManager, [:passthrough], [logged_in?: fn(_conn) -> true end] do
-    conn = conn(:get, "/")
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> SessionHelper.session_with_user
-      |> AuthenticationPlug.call(@only_opts)
-
-    assert called Passport.SessionManager.logged_in?(conn)
-    end
-  end
-
-  test "it does not check if the user is logged in if except includes action" do
-    with_mock Passport.SessionManager, [:passthrough], [logged_in?: fn(_conn) -> true end] do
-    conn = conn(:get, "/")
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> SessionHelper.session_with_user
-      |> AuthenticationPlug.call(@except_opts)
-
-    assert !called Passport.SessionManager.logged_in?(conn)
-    end
-  end
-
-  test "it uses except in preference to includes" do
-    with_mock Passport.SessionManager, [:passthrough], [logged_in?: fn(_conn) -> true end] do
-    conn = conn(:get, "/")
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> SessionHelper.session_with_user
-      |> AuthenticationPlug.call(@both_opts)
-
-    assert !called Passport.SessionManager.logged_in?(conn)
-    end
-  end
-
   # test "it returns a custom flash key" do
   #   with_mock Passport.SessionManager, [:passthrough], [logged_in?: fn(conn) -> false end] do
   #   conn = conn(:get, "/")
