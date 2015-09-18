9bec04bdad8a6e961207ad0de7500aeafa1562ee
Shankar Dhanasekaran <shankar@opendrops.com>
2015-09-19 00:16:06 +0530

    Improve validation on registration

--- lib/passport/registration_manager.ex
+++ lib/passport/registration_manager.ex
@@ -1,41 +1,32 @@
 defmodule Passport.RegistrationManager do
-  alias Ecto.Changeset
+  import Ecto.Changeset
   import Passport.Model
 
   def register(params) do
-    changeset = Changeset.cast(user_model.__struct__, params, ~w(email))
-    |> downcase_email
-    |> set_hashed_password
-    |> Changeset.validate_change(:email, &presence_validator/2)
-    |> Changeset.validate_unique(:email, on: repo)
+    changeset = user_model.changeset(user_model.__struct__, params)
 
-    case changeset.valid? do
-      true ->
-        repo.insert(changeset)
-        {:ok}
-      _ ->
-        {:error, changeset}
-    end
+    changeset = changeset
+      |> validate_format(:email, ~r/@/)
+      |> update_change(:email, &String.downcase/1)
+      |> set_hashed_password
+      |> unique_constraint(:email)
+
+    repo.insert(changeset)
+  end
+
+  def downcase_email(changeset = %{params: %{"email" => email}}) do
+    changeset
+    |> put_change(:email, String.downcase(email))
   end
 
   def set_hashed_password(changeset = %{params: %{"password" => password}}) when password != "" and password != nil do
     hashed_password = Comeonin.Bcrypt.hashpwsalt(password)
     changeset
-    |> Changeset.put_change(:crypted_password, hashed_password)
+    |> put_change(:crypted_password, hashed_password)
   end
   def set_hashed_password(changeset) do
     changeset
-    |> Changeset.add_error(:password, "is required")
+    |> add_error(:password, "is required")
   end
-
-  def downcase_email(changeset = %{params: %{"email" => email}}) when email != "" and email != nil do
-    downcased_email = String.downcase(email)
-    changeset
-      |> Changeset.put_change(:email, downcased_email)
-  end
-
-  defp presence_validator(field, nil), do: [{field, "is required"}]
-  defp presence_validator(field, ""), do: [{field, "is required"}]
-  defp presence_validator(_, _), do: []
 
 end
