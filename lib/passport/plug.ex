e16dc6827242a221df4987f968471795b655ea4f
Shankar Dhanasekaran <shankar@opendrops.com>
2016-08-03 16:57:23 +0530

    Validate user signed in status with valid user record
    Validation of whether a user signed in or not cannot be just made
    from presence of a user_id value in the session. For instance when a user gets
    deleted in the database, an invalid user_id of the deleted user may still
    exist in the session.

--- lib/passport/plug.ex
+++ lib/passport/plug.ex
@@ -4,17 +4,21 @@ defmodule Passport.Plug do
 
   def current_user(conn, _) do
     user_id = get_session(conn, :user_id)
-    user = user_id && Passport.Config.repo.get(Passport.Config.resource, user_id)
+    user = user_id && fetch_user_by_id(user_id)
     assign(conn, :current_user, user)
   end
 
   def authenticate(conn, _) do
-    case get_session(conn, :user_id) do
+    case conn.assigns[:current_user] do
       nil ->
          conn |> put_flash(:info, "You must be signed in") |> redirect(to: "/login") |> halt
       _ ->
          conn
     end
+  end
+
+  defp fetch_user_by_id(id) do
+    Passport.Config.repo.get(Passport.Config.resource, id)
   end
 
 end
