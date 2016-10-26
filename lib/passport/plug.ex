60e7adae5f35a5c60a144fb8b7b06434788727bc
Shankar Dhanasekaran <shankar@opendrops.com>
2016-10-26 21:28:33 +0530

    Quick login for test environment

--- lib/passport/plug.ex
+++ lib/passport/plug.ex
@@ -9,12 +9,29 @@ defmodule Passport.Plug do
   end
 
   def authenticate(conn, _) do
+    authenticate_env(Mix.env, conn)
+  end
+
+  defp authenticate_env(:test, conn) do
+    case conn.params["as"] do
+      nil ->
+        conn
+      id ->
+        user = fetch_user_by_id(id)
+        conn = conn
+        |> assign(:current_user, user)
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+    end
+    conn
+  end
+
+  defp authenticate_env(_, conn) do
     case conn.assigns[:current_user] do
       nil ->
         conn
         |> put_flash(:info, "You must be signed in")
         |> redirect(to: "/login")
-        |> put_session("redirect_to", conn.request_path)
         |> configure_session(renew: true)
         |> halt
       _ ->
